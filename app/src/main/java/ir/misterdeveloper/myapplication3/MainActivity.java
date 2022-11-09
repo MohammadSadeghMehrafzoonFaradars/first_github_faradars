d81c25281c64dd72be8002abab6664734101eef9
Faradars <Faradarsmehrafzoon@gmail.com>
2022-11-09 23:43:41 +0330

    toast added!!

--- app/src/main/java/ir/misterdeveloper/myapplication3/MainActivity.java
+++ app/src/main/java/ir/misterdeveloper/myapplication3/MainActivity.java
@@ -3,6 +3,7 @@ package ir.misterdeveloper.myapplication3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "test 1", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "test 2", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "test 3", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "test 4", Toast.LENGTH_SHORT).show();
     }
 }
